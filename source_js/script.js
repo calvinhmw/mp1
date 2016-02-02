8636723e63e216d81c0a2e3d4eb9284778ab648b
Mingwei Hu <calvinmwhu@gmail.com>
2016-02-01 23:59:48 -0600

    resizing has bug

--- source_js/script.js
+++ source_js/script.js
@@ -17,43 +17,130 @@
 //
 
 
+
+//
+//$(document).ready(function(){
+//    var newwidth = $(window).width();
+//    var newheight = $(window).height();
+//    $("#home").css({"height": newheight, "width": newwidth });
+//
+//    //console.log("video width is "+ videowidth);
+//    //console.log("home width is "+ homewidth);
+//    //$("#home").css({"height": videowidth });
+//
+//    //if(videoheight+300<$("#home").height()) {
+//    //    $("#home").css({"height": videoheight });
+//    //}
+//});
+//
+//
+//$(window).resize(function(){
+//    var newwidth = $(window).width();
+//    $("#home").css({"width": newwidth });
+//    var newheight = $("#background-video").height();
+//    $("#home").css({"height": newheight });
+//
+//    var videoheight = $("#background-video").height();
+//    var videowidth = $("#background-video").width();
+//    var homewidth = $("#home").width();
+//    var homeheight = $("#home").height();
+//
+//
+//    //console.log("video width is "+ videowidth);
+//    //console.log("home width is "+ homewidth);
+//
+//    //console.log("video aspact ratio is "+ videowidth/videoheight);
+//
+//
+//});
+
+
+
+
+
+
+var videoAsRatio = 1280/720;
+
+
 $(document).ready(function(){
+
     var newwidth = $(window).width();
     var newheight = $(window).height();
     $("#home").css({"height": newheight, "width": newwidth });
-
-    //console.log("video width is "+ videowidth);
-    //console.log("home width is "+ homewidth);
-    //$("#home").css({"height": videowidth });
-
-    //if(videoheight+300<$("#home").height()) {
-    //    $("#home").css({"height": videoheight });
-    //}
-});
-
-
-$(window).resize(function(){
-    var newwidth = $(window).width();
-    $("#home").css({"width": newwidth });
-    var newheight = $("#background-video").height();
-    $("#home").css({"height": newheight });
 
     var videoheight = $("#background-video").height();
     var videowidth = $("#background-video").width();
     var homewidth = $("#home").width();
     var homeheight = $("#home").height();
 
+    var homeAsRatio = homewidth/homeheight;
 
-    //console.log("video width is "+ videowidth);
-    //console.log("home width is "+ homewidth);
+    if(videoAsRatio>homeAsRatio) {
+        $("#background-video").css({"height": homeheight});
+    }else{
+        $("#background-video").css({"width": homewidth});
+    }
+});
 
-    //console.log("video aspact ratio is "+ videowidth/videoheight);
 
+// need to find a new way to resize window
+
+$(window).resize(function(){
+    var oldHomewidth = $("#home").width();
+    var oldHomeheight = $("#home").height();
+    var oldHomeAsRatio = oldHomewidth/oldHomeheight;
+
+    $("#home").css({"width": $(window).width(), "height": $(window).height()});
+
+    var videoheight = $("#background-video").height();
+    var videowidth = $("#background-video").width();
+
+    var homewidth = $("#home").width();
+    var homeheight = $("#home").height();
+    var homeAsRatio = homewidth/homeheight;
+
+    if(homeAsRatio>=videoAsRatio) {
+        $("#background-video").css({"width": homewidth});
+    }else if(homeAsRatio<videoAsRatio){
+        $("#background-video").css({"height": homeheight});
+    }
 
 });
 
-//$("#home").on({
-//    click: function() {
-//        console.log("yes");
-//    }
+
+
+
+
+//
+//$(document).ready(function(){
+//    var newwidth = $(window).width();
+//    var newheight = $(window).height();
+//    $("#home").css({"height": newheight, "width": newwidth });
+//
+//    //console.log("video width is "+ videowidth);
+//    //console.log("home width is "+ homewidth);
+//    //$("#home").css({"height": videowidth });
+//
+//    //if(videoheight+300<$("#home").height()) {
+//    //    $("#home").css({"height": videoheight });
+//    //}
+//});
+//
+//
+//$(window).resize(function(){
+//    var newwidth = $(window).width();
+//    $("#home").css({"width": newwidth });
+//    var newheight = $("#background-video").height();
+//    $("#home").css({"height": newheight });
+//
+//    var videoheight = $("#background-video").height();
+//    var videowidth = $("#background-video").width();
+//    var homewidth = $("#home").width();
+//    var homeheight = $("#home").height();
+//
+//
+//    //console.log("video width is "+ videowidth);
+//    //console.log("home width is "+ homewidth);
+//
+//    //console.log("video aspact ratio is "+ videowidth/videoheight);
 //});
